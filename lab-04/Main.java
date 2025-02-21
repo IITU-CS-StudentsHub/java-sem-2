c876296ec3a099c1220113a5a175880b9e0b0814
Max Novikoff <novikoff04@gmail.com>
2025-02-21 23:55:32 +0500

    lab 4

---
+++ lab-04/Main.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class Main {
+
+	private static void heapsort(int[] arr) {
+
+		int n = arr.length;
+
+		for (int i = n / 2 - 1; i >= 0; i--) {
+			heapify(arr, i, n);
+		}
+
+		for (int i = n - 1; i > 0; i--) {
+
+			int temp = arr[0];
+			arr[0] = arr[i];
+			arr[i] = temp;
+
+			heapify(arr, 0, i);
+		}
+
+	}
+
+	private static void heapify(int[] arr, int i, int n) {
+
+		int l = 2 * i + 1;
+		int r = 2 * i + 2;
+		int largest = i;
+
+		if (l < n && arr[l] > arr[largest]) {
+			largest = l;
+		}
+		if (r < n && arr[r] > arr[largest]) {
+			largest = r;
+		}
+		if (i != largest) {
+			int temp = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = temp;
+
+			heapify(arr, largest, n);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 5, 4, 3, 2, 1 };
+
+		heapsort(arr);
+
+		System.out.println(Arrays.toString(arr));
+	}
+}
