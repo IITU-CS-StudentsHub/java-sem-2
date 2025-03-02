08138becf5bdb6127fbebc1889d8a676a6e431c2
Max Novikoff <novikoff04@gmail.com>
2025-03-02 16:41:30 +0500

    .

--- lab-04/Main.java
+++ lab-04/Main.java
@@ -2,7 +2,11 @@ import java.util.Arrays;
 
 public class Main {
 
-	private static void heapsort(int[] arr) {
+	// TC => O(n) + O(nlogn) = O(nlogn)
+	// SC => (logn)
+	// SC => (logn)
+
+	public static void heapsort(int[] arr) {
 
 		int n = arr.length;
 
@@ -11,7 +15,6 @@ public class Main {
 		}
 
 		for (int i = n - 1; i > 0; i--) {
-
 			int temp = arr[0];
 			arr[0] = arr[i];
 			arr[i] = temp;
@@ -20,20 +23,20 @@ public class Main {
 		}
 
 	}
+	}
 
 	private static void heapify(int[] arr, int i, int n) {
-
 		int l = 2 * i + 1;
 		int r = 2 * i + 2;
 		int largest = i;
 
-		if (l < n && arr[l] > arr[largest]) {
+		if (l < n && arr[l] > arr[largest])
 			largest = l;
-		}
-		if (r < n && arr[r] > arr[largest]) {
+
+		if (r < n && arr[r] > arr[largest])
 			largest = r;
-		}
-		if (i != largest) {
+
+		if (largest != i) {
 			int temp = arr[i];
 			arr[i] = arr[largest];
 			arr[largest] = temp;
@@ -44,7 +47,9 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		int[] arr = { 5, 4, 3, 2, 1 };
+  int[] arr= {5,4,3,2,1};
+
+		System.out.println(Arrays.toString(arr));
 
 		heapsort(arr);
 
